ae9231079439a27a925b65b3603a35753dc1fe60
何冰 <625057395@qq.com>
2019-08-08 09:32:18 +0800

    测试

---
+++ pages/test/test.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<button type="primary" @tap="upload">chooseImg</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			upload(){
+				uni.chooseImage({
+					count:1,
+					sizeType:['original'],
+					success(res){
+						console.log('res',res);
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -88,7 +88,7 @@
 				<list-cell icon="icon-dizhi" iconColor="#5fcda2" title="地址管理" @eventClick="navTo('/pages/address/address')"></list-cell>
 				<list-cell icon="icon-share" iconColor="#9789f7" title="分享" tips="邀请好友赢10万大礼"></list-cell>
 				<list-cell icon="icon-pinglun-copy" iconColor="#ee883b" title="晒单" tips="晒单抢红包"></list-cell>
-				<list-cell icon="icon-shoucang_xuanzhongzhuangtai" iconColor="#54b4ef" title="我的收藏"></list-cell>
+				<list-cell icon="icon-shoucang_xuanzhongzhuangtai" iconColor="#54b4ef" title="我的收藏" @eventClick="navTo('/pages/test/test')"></list-cell>
 				<list-cell icon="icon-shezhi1" iconColor="#e07472" title="设置" border="" @eventClick="navTo('/pages/set/set')"></list-cell>
 			</view>
 		</view>
